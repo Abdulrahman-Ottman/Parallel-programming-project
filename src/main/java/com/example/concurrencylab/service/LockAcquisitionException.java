9c40c7ea2aaf6a4ff02f6cb50056f986caaa5ad2
owais <owais@gmail.com>
2026-06-20 20:44:05 +0000

    add distributed lock - requirement 7

---
+++ src/main/java/com/example/concurrencylab/service/LockAcquisitionException.java
@@ -0,0 +1,8 @@
+package com.example.concurrencylab.service;
+
+
+public class LockAcquisitionException extends RuntimeException {
+    public LockAcquisitionException(String message) {
+        super(message);
+    }
+}
